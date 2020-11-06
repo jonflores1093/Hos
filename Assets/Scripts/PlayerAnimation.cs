4200a85cdc13c69c4eab3ba07ef129c422dde2d6
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2020-11-06 14:47:46 -0600

    Added Character and some of first level

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public float framesPerSecond;
+    private SpriteRenderer spriteRenderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+        index = index % sprites.Length;
+        spriteRenderer.sprite = sprites[index];
+    }
+}
