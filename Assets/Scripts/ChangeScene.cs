50fc85e6b268606cc966be5b241e513d270412d2
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2020-11-13 18:51:08 -0600

    Added Menu, New level, and working buttons.

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+
+    }
+
+
+    public void Credits()
+    {
+
+        SceneManager.LoadScene("Credits");
+
+
+    }
+
+
+
+}
